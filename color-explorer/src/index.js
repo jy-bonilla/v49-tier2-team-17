f19b3fede05d8e5da35a3ca54e3a0b0d6b7c283a
jy-bonilla <91812771+jy-bonilla@users.noreply.github.com>
2024-06-18 19:13:36 -0400

    fixed post css error by i postcss-cli

--- color-explorer/src/index.js
+++ color-explorer/src/index.js
@@ -2,16 +2,49 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// const express = require("express");
+// const app = express();
+// const port = process.env.PORT || 3001;
+// const path = require("path");
+// const getColors = require("./helpers/get-colors");
+
+// app.use(express.static("public"));
+// app.use(express.json());
+// app.use(
+//   express.urlencoded({ extended: true })
+// );
+
+// app.get("/", async (req, res) => {
+//   res.sendFile(
+//     path.join(__dirname + "/public/")
+//   );
+// });
+
+// app.post(
+//   "/generate",
+//   async (req, res) => {
+//     const prompt = req.body.query;
+//     const colors = await getColors(
+//       prompt
+//     );
+//     res.json({
+//       colors: JSON.parse(colors),
+//     });
+//   }
+// );
+
+// app.listen(port, () => {
+//   console.log(
+//     `Example app listening at http://localhost:${port}`
+//   );
+// });
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
+);
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
